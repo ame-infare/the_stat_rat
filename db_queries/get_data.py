33bbe6ea8c62e696aab72a03816dbe9ccbb5cfec
Andrius Meškonis <ame@infare.com>
2022-03-31 21:16:59 +0300

    log in swagger (part)

--- db_queries/get_data.py
+++ db_queries/get_data.py
@@ -1,3 +1,7 @@
+import base64
+from datetime import datetime, timezone, timedelta
+from weakref import proxy
+
 import requests
 
 def get_data(session):
@@ -12,19 +16,30 @@ def get_data(session):
 
     print(response.text)
 
+def to_base64(input: str):
+    input_bytes = input.encode('ascii')
+    base64_bytes = base64.b64encode(input_bytes)
+
+    return base64_bytes.decode('ascii')
+
 def log_in(session):
+    datetime_string = datetime.now(timezone(timedelta(hours=3))).strftime('%a %b %d %Y %H:%M:%S %Z')
+    datetime_base64 = to_base64(datetime_string)
+
     query_params = {
         'response_type': 'token',
         'client_id': 'cjs-api-swagger',
         'redirect_uri': 'https://cjs-services.infare.net/current/swagger/oauth2-redirect.html',
         'scope': 'infare-services-cjv',
-        'state': 'VGh1IE1hciAzMSAyMDIyIDE2OjA5OjIyIEdNVCswMzAwIChFYXN0ZXJuIEV1cm9wZWFuIFN1bW1lciBUaW1lKQ=='
+        'state': {datetime_base64}
     }
     request = requests.Request('GET', 'https://identity.infare.net/connect/authorize', params=query_params)
     
     prepp_request = request.prepare()
     response = session.send(prepp_request)
+    pass
 
 session = requests.Session()
+session.proxies = {'https': '127.0.0.1'}
 log_in(session)
 get_data(session)
